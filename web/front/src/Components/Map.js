d0dfbad05563869e59bc1b7da7af21dc79772c4e
a22arncatsan <a22arncatsan@inspedralbes.cat>
2023-01-31 12:04:39 +0100

    popup

--- web/front/src/Components/Map.js
+++ web/front/src/Components/Map.js
@@ -1,43 +1,52 @@
-import React , { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import markerImage from './map-marker.svg';
 
-
-const center = [41.39776662950863, 2.191060344461916];
+const position = [41.39776662950863, 2.191060344461916];
 const zoom = 13;
 
 const customMarker = L.icon({
     iconUrl: markerImage,
-    iconSize: [41, 41],
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -35]
 });
 
 const MapComponent = () => {
-    const [showPopup, setShowPopup] = useState(false);
+    function LocationMarker() {
+        const [position, setPosition] = useState(null)
+        const map = useMapEvents({
+            click() {
+                map.locate()
+                console.log('locate')
+            },
+            locationfound(e) {
+                setPosition(e.latlng)
+                map.flyTo(e.latlng, map.getZoom())
+            },
+        })
+    }
 
-    return(
-        <MapContainer 
-            center={center} 
+    return (
+        <MapContainer
+            center={position}
             zoom={zoom}
             style={{ height: "100vh", width: "100vw" }}
-            
-            >
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker 
-            position={center} 
-            onClick={() => setShowPopup(true)}
-            icon={customMarker}
+            whenReady={() => {
+                console.log("This function will fire once the map is created")
+            }}
         >
-            {showPopup ? (
-            <Popup onClose={() => setShowPopup(false)}>
-                A popup for the marker in Barcelona
-            </Popup>
-            ) : null}
-        </Marker>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position} icon={customMarker}>
+                <Popup>
+                    A popup for the marker in Barcelona
+                </Popup>
+            </Marker>
+            <LocationMarker />
         </MapContainer>
     );
 };

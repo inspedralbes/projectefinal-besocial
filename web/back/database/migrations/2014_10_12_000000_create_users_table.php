75fa31f158285a60ddad2c9af6a53b0f7d5d7f24
lucascosta3 <a21luccosben@inspedralbes.cat>
2023-02-03 10:17:20 +0100

    auth laravel

--- web/back/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
             $table->string('email')->unique();
+            $table->string('phone');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

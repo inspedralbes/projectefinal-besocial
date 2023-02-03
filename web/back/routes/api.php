a605d4c23583d9f9c51bf5efa2c4f2abb4ade20a
a22arncatsan <a22arncatsan@inspedralbes.cat>
2023-02-03 13:24:48 +0100

    fix

--- web/back/routes/api.php
+++ web/back/routes/api.php
@@ -10,5 +10,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('create-organizer', [OrganizerController::class, 'createOrganizer']);
 Route::get('get-organizer', [OrganizerController::class, 'getOrganizer']);
-Route::post('create-event', [OrganizerController::class, 'createEvent']);
-Route::get('get-event', [OrganizerController::class, 'getEvent']);
+Route::post('create-event', [EventController::class, 'createEvent']);
+Route::get('get-event', [EventController::class, 'getEvent']);

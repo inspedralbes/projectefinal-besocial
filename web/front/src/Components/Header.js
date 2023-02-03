53e08212ebbc261256bf6661bc2226cb601ea977
a22arncatsan <a22arncatsan@inspedralbes.cat>
2023-02-03 12:32:23 +0100

    css

--- web/front/src/Components/Header.js
+++ web/front/src/Components/Header.js
@@ -6,14 +6,16 @@ import logo from '../Images/besocial.jpg';
 
 function Header() {
   return (
-    <div className="header">
-      <a href=""><img src={logo} alt="logo" /></a>
-      <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/login" className="buttonLogin">Login</Link>
-        <Link to="/register" className="buttonRegister">Register</Link>
+    <header>
+      <div className="header">
+        <a href="/"><img src={logo} alt="logo" /></a>
+        <div className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/login" className="buttonLogin">Login</Link>
+          <Link to="/register" className="buttonRegister">Register</Link>
+        </div>
       </div>
-    </div>
+    </header>
   )
 }
 

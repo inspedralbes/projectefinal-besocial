8127b4d2cb227803b81c11e58f7b23fb90eac89c
a22arncatsan <a22arncatsan@inspedralbes.cat>
2023-02-03 13:20:44 +0100

    backend organizer i event

---
+++ web/back/app/Http/Controllers/Api/OrganizerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Organizer;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OrganizerController extends Controller
+{
+    public function createOrganizer(Request $request)
+    {
+        $organizer = new Organizer();
+        $organizer->name = $request->name;
+        $organizer->coords = $request->coords;
+        $organizer->location = $request->location;
+        $organizer->img = $request->img;
+
+        if ($organizer->save()) {
+            return response()->json($organizer->id, Response::HTTP_CREATED);
+        }
+    }
+
+    public function getOrganizer(Request $request)
+    {
+        $organizer = Organizer::find($request->id);
+        return response()->json(["organizer" => $organizer], Response::HTTP_OK);
+    }
+}
--- web/back/routes/api.php
+++ web/back/routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\OrganizerController;
+use App\Http\Controllers\Api\EventController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('create-organizer', [OrganizerController::class, 'createOrganizer']);
+Route::get('get-organizer', [OrganizerController::class, 'getOrganizer']);
+Route::post('create-event', [OrganizerController::class, 'createEvent']);
+Route::get('get-event', [OrganizerController::class, 'getEvent']);

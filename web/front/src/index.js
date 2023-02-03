77d5fdaba9672945756ae3477b5183f6fb773080
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-02-03 11:43:52 +0100

    Merge Login in Develop

--- web/front/src/index.js
+++ web/front/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Pages/App';
-import Login from './Pages/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './Pages/App';
 import Login from './Pages/Login';

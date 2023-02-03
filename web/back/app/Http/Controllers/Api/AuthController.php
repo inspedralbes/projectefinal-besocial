98729cbfe5bf89e9f5fe48e70d4e66d2f378f56a
lucascosta3 <a21luccosben@inspedralbes.cat>
2023-02-03 11:37:04 +0100

    Auth en proceso

--- web/back/app/Http/Controllers/Api/AuthController.php
+++ web/back/app/Http/Controllers/Api/AuthController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(){
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->password = $request->password;
+        $user->save();
+
+        return response()->json([
+            "message" => "Método REGISTER" 
+        ]);
+    }
+
+    public function login(Request $request){
+        return response()->json([
+            "message" => "Método LOGIN"
+        ]);
+    }
+
+    public function userProfile(Request $request){
+        
+    }
+
+    public function logout(){
         
     }
 }

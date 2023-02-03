ad8d7c09dcf5bc1d4434dbac3a5e26b0cfbfe9d6
a22arncatsan <a22arncatsan@inspedralbes.cat>
2023-02-03 12:06:28 +0100

    correcion errores

--- web/front/src/Components/Header.js
+++ web/front/src/Components/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import "../Pages/css/style.css";
 import 'leaflet/dist/leaflet.css';
 import { Link } from "react-router-dom";
+import logo from '../Images/besocial.jpg';
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/">Home</Link>
-      <Link to="/login" className="buttonLogin">Login</Link>
-      <Link to="/register" className="buttonRegister">Register</Link>
+      <a href=""><img src={logo} alt="logo" /></a>
+      <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/login" className="buttonLogin">Login</Link>
+        <Link to="/register" className="buttonRegister">Register</Link>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Header;

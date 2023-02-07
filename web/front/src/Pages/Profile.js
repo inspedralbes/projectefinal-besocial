71ac7411a107374239e2624a46958ce7d01e4dba
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-02-07 13:01:36 +0100

    Integración back y front users

--- web/front/src/Pages/Profile.js
+++ web/front/src/Pages/Profile.js
@@ -1,23 +1,69 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import 'leaflet/dist/leaflet.css';
 import "./css/profile.css";
 import "./css/style.css";
 import Header from "../Components/Header";
-import { Outlet, Link } from "react-router-dom";
 
 function Login() {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        dataProfile();
+    },[]);
+
+    function dataProfile() {
+        let token = getCookie("cookie_token");
+
+        fetch("http://127.0.0.1:8000/api/user_profile", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer "+token
+            }
+            
+          })
+          .then(response => response.json())
+          .then(data => {
+            let userAux = [];
+            userAux.id = data.userData.id;
+            userAux.email = data.userData.email;
+            userAux.name = data.userData.name;
+
+            setUser(userAux);
+          });
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
     return (
         <div className="App">
-            <Header />
+            <Header/>
             <div className="divProfile">
                 <div className="user">
                     <div className="profile">
                         <div className="profileImg"></div>
-                        <div className="buttonSpotify">
-                            <button>Conect Spotify</button>
+                        <h2 className="nameProfile">{user.name}</h2>
+                        <div className="button">
+                            <button className="Spotify">Conect Spotify</button>
+                        </div>
+                        <div className="button">
+                            <button>Edit Profile</button>
                         </div>
                     </div>
-
                     <p className="yourTickets">Your Tickets</p>
                     <div className="tickets">
                         <div className="ticket">

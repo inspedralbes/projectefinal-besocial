7d6c31e065a2a53402231ad5366defa1760b3903
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-02-13 14:17:03 +0100

    react-bootstrap

--- web/front/src/Components/FilterMap.js
+++ web/front/src/Components/FilterMap.js
@@ -4,6 +4,9 @@ import "../Pages/css/style.css";
 import "leaflet/dist/leaflet.css";
 import MarkerComponent from "./Marker.js";
 import filtericon from "./filter.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form';
+
 
 const zoom = 8;
 let events = [];
@@ -18,148 +21,160 @@ const fechaHoy = year + "-" + month + "-" + day;
 let center = [41.8375, 1.53778];
 
 function Filter() {
-    const [nombre, setNombre] = useState("");
-    const [fecha, setFecha] = useState(fechaHoy);
+  const [nombre, setNombre] = useState("");
+  const [fecha, setFecha] = useState(fechaHoy);
 
-    const nombreFiesta = (event) => {
-        setNombre(event.target.value);
-    };
+  const nombreFiesta = (event) => {
+    setNombre(event.target.value);
+  };
 
-    const fechaFiesta = (event) => {
-        setFecha(event.target.value);
-    };
+  const fechaFiesta = (event) => {
+    setFecha(event.target.value);
+  };
 
-    const distanciaFiesta = (event) => {
-        maxDistance = event.target.value;
-        var output = document.getElementById("demo");
-        output.innerHTML = "Distance: " + parseInt(maxDistance / 1000) + " km";
-    };
+  const distanciaFiesta = (event) => {
+    maxDistance = event.target.value;
+    var output = document.getElementById("demo");
+    output.innerHTML = "Distance: " + parseInt(maxDistance / 1000) + " km";
+  };
 
-    const buscar = () => {
-        let formDataFilter = new FormData();
-        formDataFilter.append("date", fecha);
-        formDataFilter.append("search", nombre);
-        formDataFilter.append("category", "");
-        fetch("http://127.0.0.1:8000/api/get-events", {
-            method: "POST",
-            body: formDataFilter,
-        }).then((response) => response.json()).then((data) => (events = data.events));
-    };
+  const buscar = () => {
+    let formDataFilter = new FormData();
+    formDataFilter.append("date", fecha);
+    formDataFilter.append("search", nombre);
+    formDataFilter.append("category", "");
+    fetch("http://127.0.0.1:8000/api/get-events", {
+      method: "POST",
+      body: formDataFilter,
+    })
+      .then((response) => response.json())
+      .then((data) => (events = data.events));
+  };
 
-    useEffect(() => {
-        fetch("http://127.0.0.1:8000/api/get-events", {
-            method: "POST",
-        }).then((response) => response.json()).then((data) => {
-            events = data.events;
-        });
-    }, []);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/get-events", {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        events = data.events;
+      });
+  }, []);
 
-    return (
-        <div className="filtersContainer">
-            <img src={filtericon} alt="filter icon" width={50} />
-            <div className="searchbyName">
-                <label for="nombre">Buscador</label>
-                <input
-                    type="text"
-                    name="nombre"
-                    id="nombre"
-                    placeholder="Nombre del organizador, evento o ubicación"
-                    value={nombre}
-                    onChange={nombreFiesta}
-                />
-            </div>
-            <div className="searchbyDate">
-                <label for="fecha">Fecha</label>
-                <input
-                    type="date"
-                    name="fecha"
-                    id="fecha"
-                    defaultValue={fechaHoy}
-                    min={fechaHoy}
-                    onChange={fechaFiesta}
-                />
-            </div>
-            <div className="searchbyDistance">
-                <label for="distancia">Distancia</label>
-                <input
-                    type="range"
-                    name="distancia"
-                    id="distancia"
-                    min="0"
-                    max="100000"
-                    defaultValue="0"
-                    onChange={distanciaFiesta}
-                />
-                <span id="demo"></span>
-            </div>
-            <button type="submit" className="buscador" onClick={buscar}>
-                Buscar
-            </button>
-        </div>
-    );
+  return (
+    <div className="filtersContainer">
+      <img src={filtericon} alt="filter icon" width={50} />
+      <div className="searchbyName">
+        <label for="nombre">Buscador</label>
+        <input
+          type="text"
+          name="nombre"
+          id="nombre"
+          placeholder="Nombre del organizador, evento o ubicación"
+          value={nombre}
+          onChange={nombreFiesta}
+        />
+      </div>
+      <div className="searchbyDate">
+        <label for="fecha">Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          id="fecha"
+          defaultValue={fechaHoy}
+          min={fechaHoy}
+          onChange={fechaFiesta}
+        />
+      </div>
+      <div className="searchbyDistance">
+        <label for="distancia">Distancia</label>
+        <input
+          type="range"
+          name="distancia"
+          id="distancia"
+          min="0"
+          max="100000"
+          defaultValue="0"
+          onChange={distanciaFiesta}
+        />
+        <span id="demo"></span>
+      </div>
+      <div className="searchbyCategory">
+        <label for="categoria">Categoría</label>
+        <Form.Check
+          type="checkbox"
+          id="default-checkbox"
+          label="tu madre la gorda"
+        />
+      </div>
+      <button type="submit" className="buscador" onClick={buscar}>
+        Buscar
+      </button>
+    </div>
+  );
 }
 
 function Map() {
-    const [eventsMap, setEventsMap] = useState([]);
-    const L = window.L;
+  const [eventsMap, setEventsMap] = useState([]);
+  const L = window.L;
 
-    const getCoords = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                center = [position.coords.latitude, position.coords.longitude];
-            });
-        }
-    };
-
-    function MoveToLocation() {
-        const map = useMap();
-        map.flyTo(center, 13);
+  const getCoords = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        center = [position.coords.latitude, position.coords.longitude];
+      });
     }
+  };
 
-    function calcDistance(coords) {
-        coords = JSON.parse(coords);
-        let centerLatLng = L.latLng(center[0], center[1]);
-        let event = L.latLng(coords[0], coords[1]);
-        let distancia = centerLatLng.distanceTo(event);
-        return distancia;
-    }
+  function MoveToLocation() {
+    const map = useMap();
+    map.flyTo(center, 13);
+  }
 
-    function renderMarkers() {
-        let tmp = [];
-        events.forEach(function (event) {
-            let distance = calcDistance(event.coords);
-            if (parseInt(distance) < maxDistance) {
-                tmp.push(event);
-            }
-        });
-        setEventsMap(tmp);
-    };
+  function calcDistance(coords) {
+    coords = JSON.parse(coords);
+    let centerLatLng = L.latLng(center[0], center[1]);
+    let event = L.latLng(coords[0], coords[1]);
+    let distancia = centerLatLng.distanceTo(event);
+    return distancia;
+  }
 
-    useEffect(() => {
-        getCoords();
-        setInterval(function () {
-            renderMarkers();
-        }, 100);
-    }, []);
+  function renderMarkers() {
+    let tmp = [];
+    events.forEach(function (event) {
+      let distance = calcDistance(event.coords);
+      if (parseInt(distance) < maxDistance) {
+        tmp.push(event);
+      }
+    });
+    setEventsMap(tmp);
+  }
 
-    return (
-        <>
-            <MapContainer center={center} zoom={zoom}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <MoveToLocation />
-                {eventsMap.map((event) => (
-                    <MarkerComponent key={event.name} event={event} />
-                ))}
-            </MapContainer>
-        </>
-    );
+  useEffect(() => {
+    getCoords();
+    setInterval(function () {
+      renderMarkers();
+    }, 100);
+  }, []);
+
+  return (
+    <>
+      <MapContainer center={center} zoom={zoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MoveToLocation />
+        {eventsMap.map((event) => (
+          <MarkerComponent key={event.name} event={event} />
+        ))}
+      </MapContainer>
+    </>
+  );
 }
 
 export default function FilterMap() {
-    return (
-        <div className="filtersMap">
-            <Filter />
-            <Map />
-        </div>
-    );
+  return (
+    <div className="filtersMap">
+      <Filter />
+      <Map />
+    </div>
+  );
 }

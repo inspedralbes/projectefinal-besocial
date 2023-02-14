5070148635b67940b368157513a29f95da7aea59
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-02-14 12:01:02 +0100

    db fix

--- web/back/app/Http/Controllers/Api/LikeController.php
+++ web/back/app/Http/Controllers/Api/LikeController.php
@@ -5,17 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Like;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Symfony\Component\HttpFoundation\Response;
 
 class LikeController extends Controller
 {
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -24,30 +16,18 @@ class LikeController extends Controller
         ]);
 
         $like = new Like();
-        $like->userId = $request->userId;
-        $like->eventId = $request->eventId;
+        $like->id_user = $request->userId;
+        $like->id_event = $request->eventId;
         $like->save();
-        
+
         return response()->json("Like done");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
     public function show(Like $like)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Like $like)
     {
         //

56fc2505c918d6984e0e97e1bf86a8d3aeda2a57
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-04-18 09:24:46 +0200

    styling

--- web/front/src/Components/Header.js
+++ web/front/src/Components/Header.js
@@ -148,8 +148,8 @@ export default function Header() {
               <li>
                 {logged ? (
                   <Link to="/profile" className="">
-                    <div className="grid grid-cols-1 text-center">
-                      <img src={user.photo} className="rounded-full"></img>
+                    <div className="inline w-full">
+                      <img src={user.photo} className="rounded-full w-16"></img>
                       <p className="font-semibold mt-1 text-lg">Profile</p>
                     </div>
                   </Link>
@@ -158,10 +158,38 @@ export default function Header() {
                 ) : !logged ? (
                   <>
                     <Link to="/login" className="  ">
-                      Login
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                        />
+                      </svg>
+                      <p>Login</p>
                     </Link>{" "}
                     <Link to="/register" className="buttonRegister">
-                      Register
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                        />
+                      </svg>
+                      <p>Register</p>
                     </Link>
                   </>
                 ) : (

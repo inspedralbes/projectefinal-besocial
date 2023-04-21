855f6f3f8af1b6919d16318fc108d848cb5b76d8
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-04-21 10:53:58 +0200

    Link event Creator

--- web/front/src/Pages/EventCreator.js
+++ web/front/src/Pages/EventCreator.js
@@ -10,30 +10,23 @@ export default function EventCreator() {
             <div className="h-screen">
                 <Header />
                 <div className="">
-                    <div className="m-auto w-fit">
-                        <h1 className="text-white text-xl">Add your events</h1>
-                        <form className="">
-                            <label className="text-white" htmlFor="name">Name</label>
-                            <input type="text" className="" name="name"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="date">Date</label>
-                            <input type="date" className="" name="date"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="hour">Hour</label>
-                            <input type="hour" className="" name="hour"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="link">Link</label>
-                            <input type="text" className="" name="link"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="photo">Photo</label>
-                            <input type="text" className="" name="photo"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="dayOfWeek">Day of the Week</label>
-                            <input type="number" className="" name="dayOfWeek"></input>
-                            <br></br>
-                            <label className="text-white" htmlFor="categories">Categories</label>
-                            <input type="text" className="" name="categories"></input>
-                            <br></br>
+                    <div className="flex flex-col items-center justify-center m-6">
+                        <h1 className="text-white text-xl mt-3 text-center">Add your events</h1>
+                        <form className="grid grid-cols-2 gap-6 h-fit mt-3 p-8  rounded-lg">
+                            <label className="text-violet-700 items-center" htmlFor="name">Name</label>
+                            <input type="text" className="input input-bordered input-primary" name="name"></input>
+                            <label className="text-violet-700 items-center" htmlFor="date">Date</label>
+                            <input type="date" className="input input-bordered input-primary" name="date"></input>
+                            <label className="text-violet-700 items-center" htmlFor="hour">Hour</label>
+                            <input type="hour" className="input input-bordered input-primary" name="hour"></input>
+                            <label className="text-violet-700 items-center" htmlFor="link">Link</label>
+                            <input type="text" className="input input-bordered input-primary" name="link"></input>
+                            <label className="text-violet-700 items-center" htmlFor="photo">Photo</label>
+                            <input type="text" className="input input-bordered input-primary" name="photo"></input>
+                            <label className="text-violet-700 items-center" htmlFor="dayOfWeek">Day of the Week</label>
+                            <input type="number" className="input input-bordered input-primary" name="dayOfWeek"></input>
+                            <label className="text-violet-700 items-center" htmlFor="categories">Categories</label>
+                            <input type="text" className="input input-bordered input-primary" name="categories"></input>
                         </form>
                     </div>
                 </div>

2d582b5f10ec116c79e71bc25b5f509e82e548f9
lucascosta3 <a21luccosben@inspedralbes.cat>
2023-04-27 10:29:11 +0200

    cambiado likes y assistencias en el perfil para que solo salgan las de despues del dia de hoy

--- web/back/app/Http/Controllers/Api/AssistenciaController.php
+++ web/back/app/Http/Controllers/Api/AssistenciaController.php
@@ -44,7 +44,7 @@ class AssistenciaController extends Controller
                         FROM `assistencias` 
                     LEFT JOIN `events` ON assistencias.id_event = events.id
                     LEFT JOIN `organizers` ON organizers.id = events.idOrganizer
-                        where id_user =' . $id_user;
+                        where id_user =' . $id_user . ' AND events.date >= "' . date('Y-m-d') . '"';
 
         $assistUser = DB::select(DB::raw($select));
 

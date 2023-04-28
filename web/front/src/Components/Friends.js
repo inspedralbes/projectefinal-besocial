78121a12eb98bb8d7e190f60770676d9c143260a
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-04-28 09:59:04 +0200

    Friends requests funcionando

--- web/front/src/Components/Friends.js
+++ web/front/src/Components/Friends.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "../Pages/css/style.css";
 import "leaflet/dist/leaflet.css";
-import UserRequest from "./UserRequest.js";
+import Friend from "./Friend.js";
+import loadingImg from "../Images/loading_black.gif";
 
 export default function Friends() {
     const [buttonClass, setButtonClass] = useState("invisible");
@@ -9,6 +10,7 @@ export default function Friends() {
     const [logged, setLogged] = useState(false);
     const [searchUsers, setSearchUsers] = useState(new Array());
     const [friends, setFriends] = useState(new Array());
+    const [loading, setLoading] = useState(false);
     let token;
 
     useEffect(() => {
@@ -23,7 +25,7 @@ export default function Friends() {
             method: "GET",
             headers: {
                 Accept: "application/json",
-                Authorization: "Bearer " + token
+                Authorization: "Bearer " + getCookie("cookie_token")
             }
         })
             .then(response => response.json())
@@ -33,9 +35,9 @@ export default function Friends() {
                 for (let i = 0; i < data.length; i++) {
                     if (data[i].id != id) {
                         friendsAux.push(data[i]);
+                        friendsAux[friendsAux.length - 1].status = "friend";
                     }
                 }
-                console.log(friendsAux)
                 setFriends(friendsAux)
             })
 
@@ -75,7 +77,13 @@ export default function Friends() {
         }
     }
 
-    function searchUserList() {
+    function searchUserList(e) {
+        if (e != undefined) {
+            e.preventDefault();
+        }
+
+        setSearchUsers(new Array());
+        setLoading(true);
         let formData = new FormData();
         formData.append("name", searchValue);
 
@@ -87,7 +95,6 @@ export default function Friends() {
             }
         }).then((response) => response.json())
             .then((data) => {
-                console.log(data);
                 for (let i = 0; i < data[0].original.length; i++) {
                     for (let y = 0; y < data[1].original.length; y++) {
                         if (data[0].original[i].id == data[1].original[y].id) {
@@ -106,21 +113,60 @@ export default function Friends() {
                     }
 
                     for (let j = 0; j < data[3].original.length; j++) {
-                        console.log(data[0].original[i].id == data[3].original[j].id);
                         if (data[0].original[i].id == data[3].original[j].id) {
-                            data[0].original.slice(1, i);
-                            // i--;
+                            data[0].original.splice(i, 1);
+                            i--;
                             j = data[3].original.length;
                         }
                     }
                 }
                 setSearchUsers(data[0].original);
+                setLoading(false);
             });
     }
 
     const handleSearchInputChange = (event) => {
         setSearchValue(event.target.value);
     };
+
+    function requestFriend(id) {
+        let formData = new FormData();
+        formData.append("id_receiver", id);
+
+        fetch("http://127.0.0.1:8000/api/send-friend-request", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer " + token
+            },
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("correcto: " + data);
+                searchUserList();
+            })
+    }
+
+    function acceptFriend(id) {
+        let formData = new FormData();
+        formData.append("id_sender", id);
+
+        fetch("http://127.0.0.1:8000/api/accept-friend-request", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer " + getCookie("cookie_token")
+            },
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("correcto: " + data);
+                searchUserList();
+                getMyFriends();
+            })
+    }
 
     return (
         <>
@@ -133,40 +179,49 @@ export default function Friends() {
                 {logged ? (
                     <>
                         <h1>Add friend</h1>
-                        <div className="mt-1 border-2 border-slate-300 m-auto rounded-lg w-fit h-[36px] p-1 mb-3">
+                        <form onSubmit={searchUserList} className="mt-1 border-2 border-slate-300 m-auto rounded-lg w-fit h-[36px] p-1 mb-3">
                             <div className="float-left mr-2 h-fit">
                                 <svg className="w-[24px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                 </svg>
                             </div>
                             <input id="searchName" value={searchValue} onChange={handleSearchInputChange} className="float-left w-40 focus:outline-0 h-fit"></input>
-                            <button onClick={() => searchUserList()} className="float-right h-full flex items-center hover:bg-slate-300 rounded-lg w-6 ml-1">
+                            <button type="submit" className="float-right h-full flex items-center hover:bg-slate-300 rounded-lg w-6 ml-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 px-1">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                                 </svg>
                             </button>
-                        </div>
+                        </form>
                         {searchUsers.length != 0 ? (
                             <>
                                 {searchUsers.map((user, i) => (
-                                    <UserRequest user={user} i={i} />
+                                    <div className="h-[50px]" key={i}>
+                                        <div className="flex w-fit items-center float-left h-full">
+                                            <div className="rounded-full w-8 h-8 bg-center bg-cover" style={{ backgroundImage: `url("` + user.photo + `")` }}></div>
+                                            <p className="font-semibold text-lg ml-4">
+                                                {user.name}
+                                            </p>
+                                        </div>
+                                        <div className="h-full flex items-center float-right ml-5">
+                                            {user.status == null ? (
+                                                <button onClick={() => requestFriend(user.id)} className="border-2 btn-outline btn-primary hover:bg-violet-800 rounded-lg py-1 px-2 transition delay-30">Add Friend</button>
+                                            ) : user.status == "pending" ? (
+                                                <button className="border-2 text-white bg-violet-800 rounded-lg py-1 px-2" disabled>Pending</button>
+                                            ) : user.status == "request" ? (
+                                                <button onClick={() => acceptFriend(user.id)} className="border-2 btn-outline btn-primary hover:bg-violet-800 rounded-lg py-1 px-2 transition delay-30">Accept</button>
+                                            ) : (<></>)}
+                                        </div>
+                                    </div>
                                 ))}
                             </>
-                        ) : (<></>)}
-                        <hr></hr>
+                        ) : (<>
+                            {loading ? (<div className="w-full flex justify-center mb-3"><img src={loadingImg} className="w-16"></img></div>) : (<></>)}
+                            <h1 className="text-slate-600">Search your friends by name.</h1>
+                        </>)}
+                        <hr className="mt-3"></hr>
                         <h2 className="mt-3">Friends</h2>
                         {friends.map((friend, i) => (
-                            <div key={i} className="h-[50px]">
-                                <div className="flex w-fit items-center float-left h-full">
-                                    <div className="rounded-full w-8"><img
-                                        src={friend.photo}
-                                        className="rounded-full w-8 h-8"
-                                    ></img></div>
-                                    <p className="font-semibold text-lg ml-4">
-                                        {friend.name}
-                                    </p>
-                                </div>
-                            </div>
+                            <Friend key={i} user={friend} />
                         ))}
                     </>
                 ) : (<><h1>You must be logged in to add your friends!</h1></>)}

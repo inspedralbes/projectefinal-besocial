fb052e4538c39dceba23207e97d05b936c6b4ce5
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-04-28 12:04:41 +0200

    friends

--- web/front/src/Components/Friends.js
+++ web/front/src/Components/Friends.js
@@ -137,7 +137,7 @@ export default function Friends() {
             method: "POST",
             headers: {
                 Accept: "application/json",
-                Authorization: "Bearer " + token
+                Authorization: "Bearer " + getCookie("cookie_token")
             },
             body: formData,
         })

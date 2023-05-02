d1665449ad94bfe7f9f80ef0a9b7bf6e3f5c493b
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-05-02 12:55:53 +0200

    cookie

--- web/front/src/Components/YourLikes.js
+++ web/front/src/Components/YourLikes.js
@@ -27,21 +27,7 @@ function YourTickets() {
       });
   }
 
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == " ") {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
+  
 
   // function handleLikeDelete() {
   //   setNumLikes(numLikes + 1);

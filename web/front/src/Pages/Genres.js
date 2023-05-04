96b403b9f5b879a4f94fb0729df6c04907810eda
damiabreacervino <a21dambrecer@inspedralbes.cat>
2023-05-04 10:36:40 +0200

    Eliminated Card

--- web/front/src/Pages/Genres.js
+++ web/front/src/Pages/Genres.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
-import GenreCard from "../Components/GenreCard";
 import "./css/style.css";
 
 export default function Genres() {
@@ -25,17 +24,33 @@ export default function Genres() {
             })
     }
 
+    function applyGenres(e) {
+        e.preventDefault();
+    }
+
     return (
         <div className="App min-h-screen">
             <Header />
             <div className="flex items-center justify-center h-[90%] w-full vanish-title absolute">
-                <h1 className="text-slate-100 text-4xl">Escoge tus generos favoritos</h1>
+                <h1 className="text-slate-100 text-4xl">Choose your favorite genres</h1>
             </div>
-            <form>
-                <div className="p-10 grid grid-cols-4 gap-8 show-genres w-[90%] m-auto">
-                    {genres.map((genre, i) => (<GenreCard key={i} genre={genre} />))}
-                    <div className="m-auto"><button>Next</button></div>
+            <form onSubmit={applyGenres} className="show-genres pb-5">
+                <div className="m-auto w-fit pt-8"><h1 className="text-slate-100 text-4xl">Choose a maximum of 5 genres</h1></div>
+                <div className="p-10 grid grid-cols-4 gap-8 w-[90%] m-auto">
+                    {genres.map((genre, i) => (
+                        <div className="card group/close h-[150px] w-[350px] shadow-xl m-auto">
+                            <div className="bg-center bg-cover w-full h-full brightness-[0.60] blur-[1px] contrast-75 absolute z-0 rounded-md" style={{ backgroundImage: `url("` + genre.photo + `")` }}></div>
+                            <div className="card-body z-50">
+                                <label class="checkbox absolute">
+                                    <input type="checkbox" class="checkbox-input" value={genre.id}></input>
+                                    <span class="checkbox-box"></span>
+                                </label>
+                                <div className="mt-7 text-center w-fit m-auto"><h1 className="card-title text-3xl text-center z-1 text-slate-50">{genre.name}</h1></div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
+                <div className="m-auto w-fit"><button type="submit" className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded transition delay-50">Next</button></div>
             </form>
         </div>
     );

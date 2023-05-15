bc482ddc3bc7ed2725b5a2cc52f78ec1aac62a54
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-05-15 12:31:50 +0200

    design invite friends

--- web/front/src/Components/EventCard.js
+++ web/front/src/Components/EventCard.js
@@ -12,6 +12,7 @@ export default function EventCard({ event, token }) {
   const [assistBtn, setAssistBtn] = useState("Unirse");
   const [totalLikes, setTotalLikes] = useState(0);
   const [friendsAssists, setFriendsAssists] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   useEffect(() => {
     if (token) {
@@ -134,6 +135,40 @@ export default function EventCard({ event, token }) {
       });
   }
 
+  function fetchFriends() {
+    fetch(`http://127.0.0.1:8000/api/get-my-friends`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setFriends(data);
+      });
+  }
+
+  function InviteFriend(id) {
+    const friendFormData = new FormData();
+    friendFormData.append("id_receiver", id);
+    friendFormData.append("id_event", event.id);
+
+    fetch(`http://127.0.0.1:8000/api/send-invitation`, {
+      method: "POST",
+      body: friendFormData,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   return (
     <div className="card bg-base-100 min-h-[565px]">
       <figure className="h-[190px]">
@@ -146,6 +181,80 @@ export default function EventCard({ event, token }) {
           </a>
           {token && readyLike && readyLikeCount && readyAssist && (
             <>
+              <label onClick={fetchFriends} htmlFor="my-modal">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8 mr-1"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                  />
+                </svg>
+              </label>
+
+              <input type="checkbox" id="my-modal" className="modal-toggle" />
+              <div className="modal rounded-lg">
+                <div className="modal-box">
+                  {friends.length == 0 ? (
+                    <div className="text-center duration-500">
+                      <div>
+                        <svg
+                          aria-hidden="true"
+                          className="inline w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-violet-800"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  ) : (
+                    <>
+                      <h3 className="font-bold text-lg mb-4">Invite a friend</h3>
+                      <div className="max-h-[200px] overflow-auto scrollbar-thumb-violet-800 scrollbar-thin scrollbar-track-violet-200 scrollbar-rounded-md">
+                        {friends.map((friend, i) => (
+                          <label className="avatar items-center grid grid-cols-[50px,4fr,1fr] mb-3">
+                            <img
+                              className="mask mask-circle"
+                              src={friend.photo}
+                              style={{ height: "40px", width: "40px" }}
+                            ></img>
+                            <p key={i} className="font-semibold text-md m-0">
+                              {friend.name}
+                            </p>
+                            <button
+                              className="border-2 btn-outline btn-primary h-10 hover:bg-violet-800 rounded-lg py-1 px-2 transition delay-30 float-right mr-4"
+                              onClick={() => InviteFriend(friend.id)}
+                            >
+                              Invite
+                            </button>
+                          </label>
+                        ))}
+                      </div>
+                    </>
+                  )}
+                  <div className="modal-action">
+                    <label htmlFor="my-modal" className="btn">
+                      Close
+                    </label>
+                  </div>
+                </div>
+              </div>
+
               <img
                 className="w-6 mr-[2px]"
                 id={event.id}

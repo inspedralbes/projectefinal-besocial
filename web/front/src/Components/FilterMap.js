9243635f06d7d43c745eadad06d0d8bebb82da15
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-05-16 14:03:37 +0200

    Landing quasi completa per movil

--- web/front/src/Components/FilterMap.js
+++ web/front/src/Components/FilterMap.js
@@ -44,15 +44,7 @@ function Filter() {
   };
 
   const buscar = () => {
-    Swal.fire({
-      imageUrl: loading,
-      width: 120,
-      imageWidth: 50,
-      imageHeight: 50,
-      showConfirmButton: false,
-      showCancelButton: false,
-      allowOutsideClick: false,
-    });
+    document.getElementById("modal-loading").classList.remove("hidden");
     const formDataFilter = new FormData();
     if (fecha) formDataFilter.append("date", fecha);
     if (nombre) formDataFilter.append("search", nombre);
@@ -65,8 +57,10 @@ function Filter() {
       .then((data) => {
         events = data.events;
         setTimeout(function () {
-          Swal.close();
-        }, 2000);
+          window.scrollTo(0, document.body.scrollHeight);
+          document.getElementById("modal-loading").classList.add("hidden");
+        },2000);
+
       });
   };
 
@@ -126,7 +120,7 @@ function Filter() {
 
   return (
     <div className="p-4 bg-zinc-900 text-white flex gap-2 flex-col">
-      <img src={filtericon} width={55} />
+      <img src={filtericon} width={65} />
       <div className="grid mb-4">
         <label
           htmlFor="nombre"
@@ -205,13 +199,39 @@ function Filter() {
           ))}
         </select>
       </div>
-      <button
+      <label
         type="submit"
+        htmlFor="my-modal"
         className="btn btn-outline btn-primary hover:bg-violet-800 mt-8 bg-zinc-100"
         onClick={buscar}
       >
         Search
-      </button>
+      </label>
+      <input type="checkbox" id="my-modal" className="modal-toggle"></input>
+      <div className="modal" id="modal-loading">
+        <div className="modal-box w-64">
+          <div className="text-center duration-500">
+            <div>
+              <svg
+                aria-hidden="true"
+                className="inline w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-violet-800"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
       <hr className="mt-8 mb-4"></hr>
       <h3 className="font-semibold text-xl text-zinc-50 pb-1 text-center">
         Most attended events for today
@@ -357,7 +377,7 @@ function Map() {
       center={center}
       zoom={8}
       scrollWheelZoom={true}
-      className="z-0 h-full lg:h-[auto]"
+      className="z-0 h-screen lg:h-[auto]"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {eventsMap.map((event, i) => (
@@ -402,7 +422,7 @@ function List() {
   }, []);
 
   return (
-    <div className="grid p-9 lg:mt-14 lg:grid-cols-4 gap-8 items-start">
+    <div className="grid p-9 lg:mt-14 lg:grid-cols-4 gap-8 items-start scroll-smooth">
       {eventsMap.map((event, i) => (
         <EventCard key={i} event={event} token={token} />
       ))}

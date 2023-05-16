2c89e2452caedc9fe85183304674022c0f579bd4
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-05-16 11:59:28 +0200

    event creator popups

--- web/front/src/Pages/EventCreator.js
+++ web/front/src/Pages/EventCreator.js
@@ -171,47 +171,71 @@ export default function EventCreator() {
             {!orgCreated ? (
                 <div className="flex flex-col items-center justify-center mt-12">
                     <h1 className="text-white text-3xl mt-3 text-center">Create your organization</h1>
-                    <div className="grid grid-cols-2 gap-6 h-fit mt-3 p-8 rounded-lg">
+                    <div className="grid lg:grid-cols-2 lg:gap-6 h-fit mt-3 p-4 lg:p-8 rounded-lg">
                         <label className="text-violet-700 items-center flex" htmlFor="name">Name</label>
-                        <input type="text" className="input input-bordered input-primary" name="name" onChange={name}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Organizer name">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="name" onChange={name}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="address">Address</label>
-                        <input type="text" className="input input-bordered input-primary" name="address" onChange={address}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Organizer address">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="address" onChange={address}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="postal-code">Postal code</label>
-                        <input type="text" className="input input-bordered input-primary" name="postal-code" onChange={postalCode}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Organizer postal code">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="postal-code" onChange={postalCode}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="city">City</label>
-                        <input type="text" className="input input-bordered input-primary" name="city" onChange={city}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Organizer city">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="city" onChange={city}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="cordinates">Coordinates</label>
-                        <input type="text" className="input input-bordered input-primary" name="cordinates" placeholder="41.390205, 2.154007" onChange={coordinates}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Organizer coordinates (enter Google Maps, search for your location, right click on it and choose the first option to obtain the coordinates)">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="cordinates" placeholder="41.390205, 2.154007" onChange={coordinates}></input>
+                        </div>
                     </div>
                     <button type="submit" className="login-button" onClick={addOrganizer}>Add organization</button>
                 </div>
             ) : (
                 <div className="flex flex-col items-center justify-center mt-12">
                     <h1 className="text-white text-3xl mt-3 text-center">Create your events</h1>
-                    <div className="grid grid-cols-2 gap-6 h-fit mt-3 p-8 rounded-lg" id="addEventForm">
+                    <div className="grid lg:grid-cols-2 lg:gap-6 h-fit mt-3 p-4 lg:p-8 rounded-lg" id="addEventForm">
                         <label className="text-violet-700 items-center flex" htmlFor="name">Name</label>
-                        <input type="text" className="input input-bordered input-primary" name="name" onChange={eName}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Event name">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="name" onChange={eName}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="date">Date</label>
-                        <input type="date" className="input input-bordered input-primary" name="date" onChange={date}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Date of the event (if it is repetitive, inform the initial date)">
+                            <input type="date" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="date" onChange={date}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="hour">Hour</label>
-                        <input type="hour" className="input input-bordered input-primary" name="hour" onChange={hour}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Start and end time of the event">
+                            <input type="hour" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="hour" onChange={hour}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="link">Link</label>
-                        <input type="text" className="input input-bordered input-primary" name="link" onChange={link}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Link to the event">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="link" onChange={link}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="photo">Photo</label>
-                        <input type="text" className="input input-bordered input-primary" name="photo" onChange={photo}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Event photo link">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="photo" onChange={photo}></input>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="dayOfWeek">Day of the Week</label>
-                        <select className="select select-primary text-violet-700" name="dayOfWeek" onChange={dayOfWeek}>
-                            <option value=""></option>
-                            <option value="1">Monday</option>
-                            <option value="2">Tuesday</option>
-                            <option value="3">Wednesday</option>
-                            <option value="4">Thursday</option>
-                            <option value="5">Friday</option>
-                            <option value="6">Saturday</option>
-                            <option value="7">Sunday</option>
-                        </select>
+                        <div className="tooltip lg:tooltip-right" data-tip="If the event is repetitive, choose the day for the repetition">
+                            <select className="select select-primary text-violet-700 mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="dayOfWeek" onChange={dayOfWeek}>
+                                <option value=""></option>
+                                <option value="1">Monday</option>
+                                <option value="2">Tuesday</option>
+                                <option value="3">Wednesday</option>
+                                <option value="4">Thursday</option>
+                                <option value="5">Friday</option>
+                                <option value="6">Saturday</option>
+                                <option value="7">Sunday</option>
+                            </select>
+                        </div>
                         <label className="text-violet-700 items-center flex" htmlFor="categories">Categories</label>
-                        <input type="text" className="input input-bordered input-primary" name="categories" placeholder="Category 1, Category 2" onChange={categories}></input>
+                        <div className="tooltip lg:tooltip-right" data-tip="Write the categories of the event separated by commas">
+                            <input type="text" className="input input-bordered input-primary mb-4 lg:mb-0 mt-1 lg:mr-1 w-[99%]" name="categories" placeholder="Category 1, Category 2" onChange={categories}></input>
+                        </div>
                     </div>
                     <button type="submit" className="login-button" onClick={addEvent}>Add event</button>
                 </div>

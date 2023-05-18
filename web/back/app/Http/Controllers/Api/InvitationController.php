59e7346570d65bc63617df1176e507310bd769f3
lucascosta3 <a21luccosben@inspedralbes.cat>
2023-05-18 12:32:14 +0200

    el endpoint que te devuelve las invitaciones te devuelve tambien el nombre del organizador

--- web/back/app/Http/Controllers/Api/InvitationController.php
+++ web/back/app/Http/Controllers/Api/InvitationController.php
@@ -93,8 +93,11 @@ class InvitationController extends Controller
     public function getMyInvitations()
     {
         $id_user = auth()->user()->id;
-        $select = 'SELECT users.photo, users.name, users.id, id_event, events.name as eventName, events.link FROM invitations LEFT JOIN users on (users.id=id_receiver or users.id=id_sender) 
-        LEFT JOIN events on events.id = id_event WHERE id_receiver = ' . $id_user . ' AND status=0 AND users.id != ' . $id_user;
+        $select = 'SELECT users.photo, users.name, users.id, id_event, events.name as eventName, events.link, organizers.name as organizerName 
+        FROM invitations LEFT JOIN users on (users.id=id_receiver or users.id=id_sender) 
+        LEFT JOIN events on events.id = id_event 
+        LEFT JOIN organizers on organizers.id = events.idOrganizer 
+        WHERE id_receiver = ' . $id_user . ' AND status=0 AND users.id != ' . $id_user;
         $select = DB::select(DB::raw($select));
         return response()->json($select);
     }
